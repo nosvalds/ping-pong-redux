21faab6d6437b73d4eae6bac5ea154bf6b86994e
Nik Osvalds <nosvalds@gmail.com>
2020-06-08 11:11:20 +0100

    add initial state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; // renders React into the browser, could replace with ReactNative for an iOS app for example
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// Redux stuff lives here
+
+// initial state for the entire App
+const initial = {
+  player1: 0,
+  player2: 0,
+};
+
+
+// Strict mode gives you extra validation and warnings when you're building things. Newer is the latest 3mo
+// gets the root element from index.html
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') 
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
