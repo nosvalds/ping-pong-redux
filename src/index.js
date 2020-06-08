c494f004a021b97c3ea6e53287b7e551a99e72fd
Nik Osvalds <nosvalds@gmail.com>
2020-06-08 13:37:14 +0100

    redux initial setup of state and rendering

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom'; // renders React into the browser, could replace with ReactNative for an iOS app for example
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
 
 // Redux stuff lives here
 
@@ -11,13 +12,31 @@ const initial = {
   player2: 0,
 };
 
+// reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    default: return state;
+  }
+}
 
-// Strict mode gives you extra validation and warnings when you're building things. Newer is the latest 3mo
-// gets the root element from index.html
+// store set up
+const store = createStore(reducer, initial);
 
-ReactDOM.render(
-  <React.StrictMode> 
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root') 
-);
+// rendering
+
+const render = () => {
+  let state = store.getState();
+
+  // Strict mode gives you extra validation and warnings when you're building things. Newer is the latest 3mo
+  // gets the root element from index.html
+
+  ReactDOM.render(
+    <React.StrictMode> 
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root') 
+  );
+}
+
+store.subscribe(render); // render when state changes
+render(); // render when page loads using initial state
