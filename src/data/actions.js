529931191d830b8b5724298f5d66d499c1a4da16
Nik Osvalds <nosvalds@gmail.com>
2020-06-10 10:24:41 +0100

    implement dispatching from react-redux for app

---
+++ src/data/actions.js
@@ -0,0 +1,23 @@
+export const incrementP1 = () => {
+    return {
+        type: "INCREMENT_P1"
+    }
+}
+
+export const incrementP2 = () => {
+    return {
+        type: "INCREMENT_P2"
+    }
+}
+
+export const reset = () => {
+    return {
+        type: "RESET"
+    }
+}
+
+export const toggleLanguage = () => {
+    return {
+        type: "TOGGLE_LANGUAGE"
+    }
+}
