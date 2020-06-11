ccf425e814e37dc05fde788db92573292f97c770
Nik Osvalds <nosvalds@gmail.com>
2020-06-11 15:17:35 +0100

    move gameplay into its own component to clean up App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import Header from "../Header";
-import ScoreCard1 from "../ScoreCard/ScoreCard1";
-import ScoreCard2 from "../ScoreCard/ScoreCard2";
-import Winner from '../Winner'
 import GameHistory from "../GameHistory";
-import translations from '../translations.json'
 import Settings from "../Settings";
 import Loading from "../Loading";
 import LanguageSelection from "../LanguageSelection";
+import GamePlay from "../GamePlay";
 
 const App = ({ 
-  handleNewGame, 
-  language,
   showSettings 
 }) => (
-    <React.Fragment>
+    <>
         { /* language selection */}
         <LanguageSelection />
 
@@ -23,43 +18,15 @@ const App = ({
 
         {/* Settings or Gameplay */}
         { showSettings ? 
-          // render the settings component
           <Settings />
         : 
-          <>
-            {/* gameplay scores section */}
-            <div className="row mb-4">
-              <ScoreCard1 
-                player={ 1 }
-              />
-              <ScoreCard2 
-                player={ 2 }
-              />
-            </div>
-        
-            { /* winner message */}
-            <Winner />
-
-            <hr />
-
-            { /* New Game button */}
-            <div className="d-flex justify-content-between">
-              <button 
-                className="btn btn-danger"
-                onClick={ handleNewGame }
-              >
-                { translations.new_game[language] }
-              </button>
-            </div>
-
-            <hr />
-          </>
+          <GamePlay />
         }
-        {/* Game History */}
+        {/* Game History, wrap in loading so we show loading bar until history is loaded from the API */}
         <Loading>
           <GameHistory />
         </Loading>
-    </React.Fragment>
+    </>
 );
 
 export default App;
