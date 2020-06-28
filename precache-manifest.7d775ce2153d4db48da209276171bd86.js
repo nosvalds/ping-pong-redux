a1e531226d0f20564aeb670a3d9177b20a5f2b17
Nik Osvalds <nosvalds@gmail.com>
2020-06-28 22:49:52 +0100

    Updates

---
+++ precache-manifest.7d775ce2153d4db48da209276171bd86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ffb08ac128a04ce4a508abdb4cabd6",
+    "url": "/ping-pong-redux/index.html"
+  },
+  {
+    "revision": "0cd4d16176d1987db1be",
+    "url": "/ping-pong-redux/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0817c589a25837f335cf",
+    "url": "/ping-pong-redux/static/js/2.81126714.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ping-pong-redux/static/js/2.81126714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cd4d16176d1987db1be",
+    "url": "/ping-pong-redux/static/js/main.72b9469d.chunk.js"
+  },
+  {
+    "revision": "64faec5bc556b85c626f",
+    "url": "/ping-pong-redux/static/js/runtime-main.8007d502.js"
+  }
+]);
